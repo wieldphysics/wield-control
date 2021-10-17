13d5891f05e405b1c6b0e3650c5bcdf50edbe611
Lee McCuller <Lee.McCuller@gmail.com>
2021-10-17 18:19:35 -0500

    license updates

--- src/wavestate/control/statespace/dense/test/SS_inv.py
+++ src/wavestate/control/statespace/dense/test/SS_inv.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: © 2021 Massachusetts Institute of Technology.
+# SPDX-FileCopyrightText: © 2021 Lee McCuller <mcculler@mit.edu>
+# NOTICE: authors should document their contributions in concisely in NOTICE
+# with details inline in source files, comments, and docstrings.
+"""
+"""
 from wavestate.utilities.ipynb_lazy import *
 from wavestate.utilities.ipynb.sympy import *
 from transient.TFmath import order_reduce
