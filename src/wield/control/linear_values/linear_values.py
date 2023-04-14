c98c2a16907f9342296094fa390b2ae13430183a
Lee McCuller <Lee.McCuller@gmail.com>
2023-04-14 13:16:53 -0500

    wield cfg file

--- src/wield/control/linear_values/linear_values.py
+++ src/wield/control/linear_values/linear_values.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: © 2022 California Institute of Technology.
 # SPDX-FileCopyrightText: © 2021 Massachusetts Institute of Technology.
-# SPDX-FileCopyrightText: © 2021 Lee McCuller <mcculler@mit.edu>
+# SPDX-FileCopyrightText: © 2021 Lee McCuller <mcculler@caltech.edu>
 # NOTICE: authors should document their contributions in concisely in NOTICE
 # with details inline in source files, comments, and docstrings.
 """
