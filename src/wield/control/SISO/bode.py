2fa4138b122c13102dcb3538683ecc0b9ae2bfd4
Jeffrey Wack <jeffwack111@gmail.com>
2024-08-30 17:54:00 -0700

    bode plotter for siso systems using vega altair

--- src/wield/control/SISO/bode.py
+++ src/wield/control/SISO/bode.py
@@ -16,36 +16,42 @@ from wield.control import SISO
 from wield.control.ss_bare.ss import BareStateSpace as RawStateSpace
 from wield.control.ss_bare.ssprint import print_dense_nonzero
 
+import altair as alt
+import pandas as pd
+
+def zp_frequencies(z,p):
+    z = z[z.imag >= 0]
+    p = p[p.imag >= 0]
+
+    F_include = np.sort(np.concatenate(
+        [
+            z.imag,
+            p.imag,
+            z.imag - abs(z.real) - 1e-6,
+            z.imag + abs(z.real) + 1e-6,
+            p.imag - abs(p.real) - 1e-6,
+            p.imag + abs(p.real) + 1e-6,
+        ]
+    )) / (2 * np.pi)
+
+    F_include = F_include[F_include > 0]
+
+    return F_include
+
 def bode(
     sys,
-    axB=None,
     F_Hz=None,
     omega_limits=None,
     include_zp = True,
-    label=None,
-    **kwargs
+    label="SISO system",
 ):
     """
     sys should be a wield.control.SISO object
     """
-    if axB is None:
-        axB = mplfigB(Nrows = 2)
 
     if include_zp:
         z, p = sys._zp
-        z = z[z.imag > 0]
-        p = p[p.imag > 0]
-        F_include = np.sort(np.concatenate(
-            [
-                z.imag,
-                p.imag,
-                z.imag - abs(z.real) - 1e-6,
-                z.imag + abs(z.real) + 1e-6,
-                p.imag - abs(p.real) - 1e-6,
-                p.imag + abs(p.real) + 1e-6,
-            ]
-        )) / (2 * np.pi)
-        F_include = F_include[F_include > 0]
+        F_include = zp_frequencies(z,p)
 
         if omega_limits is None:
             omega_limits = (
@@ -57,100 +63,101 @@ def bode(
         F_Hz = np.geomspace(omega_limits[0], omega_limits[1], 1000)
 
     if include_zp:
-        print("F_include", F_include)
         F_Hz = np.sort(np.concatenate([F_Hz, F_include]))
 
     fr = sys.fresponse(f=F_Hz)
-    axB.ax0.loglog(*fr.fplot_mag, label=label, **kwargs)
-    if hasattr(axB, 'ax1'):
-        axB.ax1.semilogx(*fr.fplot_deg225, label=label, **kwargs)
-    return axB
 
+    alldata = pd.DataFrame({'frequency (Hz)':fr.f,'magnitude':fr.mag,'phase':fr.deg,'label':label})
 
-@contextlib.contextmanager
-def multi_bode(
-    axB,
+    magnitudechart = alt.Chart(alldata).mark_line().encode(
+    x=alt.X('frequency (Hz):Q').scale(type="log"),
+    y=alt.Y('magnitude:Q').scale(type="log"),
+    color='label:N',
+    ).properties(
+        width=400,
+        height=200
+    )
+
+    phasechart = alt.Chart(alldata).mark_line().encode(
+        x=alt.X('frequency (Hz):Q').scale(type="log"),
+        y=alt.Y('phase:Q'),
+        color='label:N',
+    ).properties(
+        width=400,
+        height=100
+    )
+
+    chart = alt.vconcat(magnitudechart,phasechart)
+
+    return chart
+
+#@contextlib.contextmanager
+def multi_bode(syslist,
+    labels = None,
     F_Hz = None,
+    omega_limits=None,
     include_zp = True,
-    **kwargs
 ):
     """
     Plot multiple bode plots at once.
 
     The main advantage is that the scale of the axes can be better auto-determined to capture all known poles and zeros.
-    The input arguments should all be dictionaries containing the bode kwargs.
-
-    the usage should be
-
-    the kwargs on the top call are passed into all sub calls
-
-    with multi_bode(axB=axB, **kw_cmn) as bode:
-        bode(sisoA, label='A', **kw)
-        bode(sisoB, label='B', **kw)
-        bode(sisoC, label='C', **kw)
-    axB.save('figname.pdf')
     """
-    bode_arg_kwarg_list = []
-    F_includes = []
 
-    def bode_sys(
-        sys,
-        **kwargs
-    ):
-        """
-        Just extract and return the bode system
-        """
-        return sys
+    zlist = np.array([])
+    plist = np.array([])
 
-    def bode_plot(*args, **kwargs):
-        sys = bode_sys(*args, **kwargs)
+    if include_zp:
+        for sys in syslist:
+            z, p = sys._zp
+            zlist = np.concatenate((zlist,z))
+            plist = np.concatenate((plist,p))
 
-        z, p = sys._zp
-        z = z[z.imag > 0]
-        p = p[p.imag > 0]
-        F_include = np.sort(np.concatenate(
-            [
-                z.imag,
-                p.imag,
-                z.imag - abs(z.real) - 1e-6,
-                z.imag + abs(z.real) + 1e-6,
-                p.imag - abs(p.real) - 1e-6,
-                p.imag + abs(p.real) + 1e-6,
-            ]
-        )) / (2 * np.pi)
-        F_include = F_include[F_include > 0]
-        F_includes.append(F_include)
+        F_include = zp_frequencies(zlist,plist)
 
-        bode_arg_kwarg_list.append(
-            (args, kwargs)
-        )
-
-    yield bode_plot
-
-    F_include = np.sort(np.concatenate(F_includes))
+        if omega_limits is None:
+            omega_limits = (
+                (F_include[0] + 1e-6) / 3,
+                F_include[-1] * 3
+            )
 
     if F_Hz is None:
-        F_Hz = np.geomspace(
-            (F_include[0] + 1e-6) / 3,
-            F_include[-1] * 3,
-            300
-        )
+        F_Hz = np.geomspace(omega_limits[0], omega_limits[1], 1000)
 
+    #this makes include_zp irrelevant if F_Hz or omega_limits are passed
     F_include = F_include[F_include < F_Hz[-1]]
     F_include = F_include[F_include > F_Hz[0]]
 
     if include_zp:
         F_Hz = np.sort(np.concatenate([F_Hz, F_include]))
 
-    for arg, kwarg in bode_arg_kwarg_list:
-        kw = dict(**kwargs)
-        kw.update(kwarg)
-        bode(
-            *arg,
-            axB=axB,
-            F_Hz=F_Hz,
-            omega_limits=None,
-            include_zp = False,
-            **kw
-        )
-    return
+    fresponses = [sys.fresponse(f=F_Hz) for sys in syslist]
+    
+    if labels == None:
+        labels = [f"system {ii}" for ii in range(len(syslist))]
+
+    dataframes = [pd.DataFrame({'frequency (Hz)':fr.f,'magnitude':fr.mag,'phase':fr.deg,'label':label}) for (fr,label) in zip(fresponses,labels)]
+    
+    alldata = pd.concat(dataframes)
+
+    magnitudechart = alt.Chart(alldata).mark_line().encode(
+    x=alt.X('frequency (Hz):Q').scale(type="log"),
+    y=alt.Y('magnitude:Q').scale(type="log"),
+    color='label:N',
+    ).properties(
+        width=400,
+        height=200
+    )
+
+    phasechart = alt.Chart(alldata).mark_line().encode(
+        x=alt.X('frequency (Hz):Q').scale(type="log"),
+        y=alt.Y('phase:Q'),
+        color='label:N',
+    ).properties(
+        width=400,
+        height=100
+    )
+
+    chart = alt.vconcat(magnitudechart,phasechart)
+
+    return chart
